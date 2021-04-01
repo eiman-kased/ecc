0175898866377ec4439e3dfac684d1cbb7827a06
Eiman Kased <eiman.kased@gmail.com>
2021-03-31 20:05:39 -0400

    added the htaccess file, and setBasePath

--- index.php
+++ index.php
@@ -1,17 +1,19 @@
 <?php
 
-use \Psr\Http\Message\ServerRequestInterface as Request;
-use \Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
 
-require 'vendor/autoload.php';
+require __DIR__ . '/vendor/autoload.php';
+$app = AppFactory::create();
+$app->setBasePath('/ecc');
 
-$app = new \Slim\App;
-$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
-	$name = $args['name'];
-	$response->getBody()->write("Hello, $name");
 
+$app->get('/', function (Request $request, Response $response, $args) {
+	$response->getBody()->write("Hello world!");
 	return $response;
 });
+
 $app->run();
 
 
